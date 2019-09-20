bfbcf80129710f6cc0ec551e6ce5b89ccf248562
Iamverygood88 <e_sindiukova@fanshaweonline.ca>
2019-09-20 18:47:54 -0400

    fineshed version

--- video/js/portfolio.js
+++
@@ -1,7 +0,0 @@
-//back-button on portfolio page
-let portfolioBackBtn = document.querySelector(".intro-back-btn");
-
-portfolioBackBtn.addEventListener("click", function(){
-  window.history.back();
-  console.log("dsfds");
-});
